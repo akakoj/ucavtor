5d11d1f05e8f4390ea0a32c1d10b6465b5308930
Evgeniy Troynov <troinof@gmail.com>
2018-10-21 16:10:05 +0300

    #master. change config webpack

---
+++ packages/admin/webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+      },
+    ]
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+}
